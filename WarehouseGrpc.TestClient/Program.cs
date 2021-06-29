93b5b5aeaab5289c1d4a032a8636ae2d80a19f95
pro5 <pro5@SRV-FRZ-WEB>
2021-06-29 21:47:20 +0300

    Добавлены методы выгрузки данных стримом

--- WarehouseGrpc.TestClient/Program.cs
+++ WarehouseGrpc.TestClient/Program.cs
@@ -19,8 +19,16 @@ namespace WarehouseGrpc.TestClient
             Console.WriteLine(transporter.WorkOut);
             var gate = await client.GetGateAsync(id);
             Console.WriteLine(gate.CarNumber);
+
+            var gates = client.GetGates(id);
+            Gate cGate;
+            while (gates.ResponseStream.MoveNext(System.Threading.CancellationToken.None).Result)
+            {
+                cGate = gates.ResponseStream.Current;
+                Console.WriteLine(cGate.CarNumber);
+            };
         }
 
-        private static readonly GrpcChannel GrpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+        private static readonly GrpcChannel GrpcChannel = GrpcChannel.ForAddress("https://localhost:5555");
     }
 }
--- WarehouseGrpc/Services/WarehouseService.cs
+++ WarehouseGrpc/Services/WarehouseService.cs
@@ -16,6 +16,8 @@ namespace WarehouseGrpc
             _logger = logger;
         }
 
+        private readonly Random Random = new Random();
+
         public override Task<Crane> GetCrane(ID request, ServerCallContext context)
         {
             var crane = new Crane
@@ -26,6 +28,29 @@ namespace WarehouseGrpc
                 QueueLength = 0
             };
             return Task.FromResult(crane);
+        }
+
+        public override Task GetCranes(ID request, IServerStreamWriter<Crane> responseStream, ServerCallContext context)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Crane crane = new Crane
+                {
+                    CraneNumber = i,
+                    CraneState = Random.Next(4) + 1
+                };
+                if (crane.CraneState < 3)
+                {
+                    crane.QueueLength = Random.Next(10);
+                }
+                else
+                {
+                    crane.ErrorTime = Timestamp.FromDateTime(
+                        DateTime.UtcNow.AddMinutes(-Random.Next(240)));
+                }
+                responseStream.WriteAsync(crane);
+            }
+            return Task.CompletedTask;
         }
 
         public override Task<Transporter> GetTransporter(ID request, ServerCallContext context)
@@ -41,20 +66,71 @@ namespace WarehouseGrpc
             return Task.FromResult(transporter);
         }
 
+        public override Task GetTransporters(ID request, IServerStreamWriter<Transporter> responseStream, ServerCallContext context)
+        {
+            for (int i = 1; i <= 2; i++)
+            {
+                Transporter transporter = new Transporter
+                {
+                    TransporterNumber = i,
+                    WorkIn = Random.Next() % 2 == 0,
+                    WorkOut = Random.Next() % 2 == 0,
+                    ErrorIn = Random.Next() % 2 == 0,
+                    ErrorOut = Random.Next() % 2 == 0
+                };
+                responseStream.WriteAsync(transporter);
+            }
+            return Task.CompletedTask;
+        }
+
         public override Task<Gate> GetGate(ID request, ServerCallContext context)
         {
             var gate = new Gate
             {
                 GateNumber = request.Id,
-                GateOpen = true,
-                CarPresent = true,
-                CarBeginTime = Timestamp.FromDateTime(DateTime.UtcNow.AddHours(-1)),
-                CarNumber = "о000оо00",
-                OperationType = 1,
-                AlarmPalletCount = 0,
-                PalletCount = 5
+                CarPresent = Random.Next() % 2 == 0
             };
+            if (gate.CarPresent)
+            {
+                gate.GateOpen = Random.Next() % 2 == 0;
+                gate.CarBeginTime = Timestamp.FromDateTime(DateTime.UtcNow.AddMinutes(Random.Next()));
+                gate.CarNumber = "о000оо00";
+                gate.OperationType = Random.Next() % 2 == 0 ? 1 : 2;
+                gate.PalletCount = Random.Next(41);
+                gate.AlarmPalletCount = Random.Next(10);
+                if (gate.AlarmPalletCount < 5 || gate.AlarmPalletCount > gate.PalletCount)
+                {
+                    gate.AlarmPalletCount = 0;
+                }
+            }
             return Task.FromResult(gate);
+        }
+
+        public override Task GetGates(ID request, IServerStreamWriter<Gate> responseStream, ServerCallContext context)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Gate gate = new Gate();
+                gate.GateNumber = i;
+                gate.CarPresent = Random.Next() % 2 == 0;
+                if (gate.CarPresent)
+                {
+                    gate.CarNumber = "a123bc178";
+                    gate.CarBeginTime = Timestamp.FromDateTime(
+                        DateTime.UtcNow.AddMinutes(-Random.Next(240)));
+                    gate.GateOpen = Random.Next() % 2 == 0;
+                    gate.OperationType = Random.Next() % 2 == 0 ? 1 : 2;
+                    gate.PalletCount = Random.Next(41);
+                    gate.AlarmPalletCount = Random.Next(10);
+                    if (gate.AlarmPalletCount < 5 ||
+                        gate.AlarmPalletCount > gate.PalletCount)
+                    {
+                        gate.AlarmPalletCount = 0;
+                    }
+                }
+                responseStream.WriteAsync(gate);
+            }
+            return Task.CompletedTask;
         }
 
         public override Task<Duration> GetCraneErrorDuration(Crane request, ServerCallContext context)
