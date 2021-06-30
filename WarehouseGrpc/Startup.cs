40d38892b6a992218e9f7e047ca7b8c9fc743228
pro5 <pro5@SRV-FRZ-WEB>
2021-06-30 14:48:38 +0300

    Добавлена поддержка reflections

--- WarehouseGrpc/Startup.cs
+++ WarehouseGrpc/Startup.cs
@@ -17,6 +17,7 @@ namespace WarehouseGrpc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddGrpcReflection();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +33,10 @@ namespace WarehouseGrpc
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<WarehouseService>();
+                if (env.IsDevelopment())
+                {
+                    endpoints.MapGrpcReflectionService();
+                }
 
                 endpoints.MapGet("/", async context =>
                 {
