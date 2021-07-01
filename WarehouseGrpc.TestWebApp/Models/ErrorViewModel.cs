857b150ea68b19af1e2fb1ff7abab3d1c88ac782
pro5 <pro5@SRV-FRZ-WEB>
2021-07-01 17:59:21 +0300

    Добавлено тестовое web-приложение ASP.NET Core

---
+++ WarehouseGrpc.TestWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WarehouseGrpc.TestWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
